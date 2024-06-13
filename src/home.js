f6e80ffaf5633d7109434e877bc072a7d1d077a1
acl13 <128934431+acl13@users.noreply.github.com>
2024-06-13 13:59:49 -0400

    add tater-tots.jpeg to hompe page

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-// //import Tots from './tater-tots.jpeg';
+import Img from './tater-tots.jpeg';
 
 function createHomePage() {
     const element = document.createElement('div');
@@ -8,12 +8,12 @@ function createHomePage() {
     title.textContent = 'Tasty Tots';
     description.textContent = `Get ready to experience the tastiest tater tots you've ever had! Our tots come in a variety of flavors, sure to please every palate.`
 
- //   const img = new Image();
-  //  img.src = Tots;
+    const tots = new Image();
+    tots.src = Img;
 
     element.appendChild(title);
     element.appendChild(description);
-  //  element.appendChild(img);
+     element.appendChild(tots);
 
     return element;
 
