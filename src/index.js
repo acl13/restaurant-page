5ddfa0d07fc6396ef767da067a84674ef7e3ccd2
acl13 <128934431+acl13@users.noreply.github.com>
2024-06-13 15:19:19 -0400

    update page content on button click event

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-//import { createHomePage } from './home';
-//import { createMenu } from './menu'
+import { createHomePage } from './home';
+import { createMenu } from './menu'
 import { createContactPage } from './contact';
 import './style.css';
 
-// const content = document.getElementById('content');
-// content.appendChild(createHomePage());
-
-// const content = document.getElementById('content');
-// content.appendChild(createMenu());
-
 const content = document.getElementById('content');
-content.appendChild(createContactPage());
+const home = document.getElementById('home');
+const menu = document.getElementById('menu');
+const contact = document.getElementById('contact');
+
+content.appendChild(createHomePage());
+
+home.addEventListener('click', () => {
+    content.innerHTML = '';
+    content.appendChild(createHomePage());
+})
+
+menu.addEventListener('click', () => {
+    content.innerHTML = '';
+    content.appendChild(createMenu());
+})
+
+contact.addEventListener('click', () => {
+    content.innerHTML = '';
+    content.appendChild(createContactPage());
+})
+
