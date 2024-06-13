62f5f79a36c4273dbba0f163e14ee55b1af50ad4
acl13 <128934431+acl13@users.noreply.github.com>
2024-06-13 15:06:50 -0400

    import modules to create menu and conatct pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import { createHomePage } from './home';
+//import { createHomePage } from './home';
+//import { createMenu } from './menu'
+import { createContactPage } from './contact';
 import './style.css';
 
+// const content = document.getElementById('content');
+// content.appendChild(createHomePage());
+
+// const content = document.getElementById('content');
+// content.appendChild(createMenu());
+
 const content = document.getElementById('content');
-content.appendChild(createHomePage());
+content.appendChild(createContactPage());
